889fc969a890eb1affdb43b79f25f49736bac6ab
Magno Martinez <mmartinezbu@ia.com.mx>
2025-01-28 14:56:01 -0500

    [upd]: subir todo lo correspondiente al despliegue

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 # Exit on error
 set -o errexit
 
+python -m pip install --upgrade pip
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
+
+# create static directory if it doesn't exist
+mkdir -p static
 
 # Apply any outstanding database migrations
 python manage.py migrate
